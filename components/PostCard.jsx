5eab2e0e17ca01ce2a4985dd5b62846af25457a2
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-12-22 16:05:15 +0900

    creating and styling the post widget

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -36,7 +36,7 @@ const PostCard = ({ post }) => {
           </span>
         </div>
       </div>
-      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:p-20 mb-8">{post.exerpt}</p>
+      <p className="text-center text-lg text-gray-700 font-normal px-4 lg:p-2 mb-8">{post.exerpt}</p>
       <div className="text-center">
         <Link href={`/post/${post.slug}`}>
             <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
