d7c437307cd34a3c012fa49c4e6ecfcc0c5c4540
Malene Thisgaard <malene.thisgaard@gmail.com>
2022-12-31 15:17:14 +0900

    add featured posts to post page

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router';
 import { getPosts, getPostDetails } from '../../services';
+import { FeaturedPosts } from '../../sections'
 import { PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader } from '../../components';
 
 const PostDetails = ({ post }) => {
@@ -12,6 +13,7 @@ const PostDetails = ({ post }) => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           <PostDetail post={post} />
