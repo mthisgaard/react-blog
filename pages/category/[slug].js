48cdf54f58d6c2321ddafec529a7da242e23d3ac
Malene Thisgaard <malene.thisgaard@gmail.com>
2023-01-10 11:27:23 +0900

    add featured posts to category search page

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { getCategories, getCategoryPost } from '../../services';
-import { PostCard, Categories, Loader } from '../../components';
+import { PostCard, Categories, Loader, PostWidget } from '../../components';
+import { FeaturedPosts } from '../../sections'
 
 const CategoryPost = ({ posts }) => {
   const router = useRouter();
@@ -12,6 +13,7 @@ const CategoryPost = ({ posts }) => {
 
   return (
     <div className="container mx-auto px-10 mb-8">
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="col-span-1 lg:col-span-8">
           {posts.map((post, index) => (
@@ -20,6 +22,7 @@ const CategoryPost = ({ posts }) => {
         </div>
         <div className="col-span-1 lg:col-span-4">
           <div className="relative lg:sticky top-8">
+            <PostWidget />
             <Categories />
           </div>
         </div>
